0df31feb431b508c2bfc22b9768e4cdc8da8828c
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-05-29 17:02:08 +0200

    Initial Commit

---
+++ VIP_01_Beers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIP_01_Beers
+//
+//  Created by Jorge Fuentes Casillas on 29/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
